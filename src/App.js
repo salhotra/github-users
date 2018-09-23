6e7dcc7aa931170b352d2b8aa9ffd4d09d01631b
Nishant Salhotra <nishantsalhotra.ns@gmail.com>
2018-09-23 22:32:47 +0530

    Chore: Add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
+import routes from './config/routes';
+
+import Home from './routes/Home';
+import User from './routes/User';
+import NotFound from './routes/NotFound';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path={routes.HOME} component={Home} />
+      <Route path={routes.USER} component={User} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+);
 
 export default App;
---
+++ src/config/routes.js
@@ -0,0 +1,6 @@
+const routes = {
+  HOME: '/',
+  USER: '/users/:username',
+};
+
+export default routes;
